e7bb55b04cb3e206c9a2b5deeae09113f6b7d598
Mukum <laughingmukum@yahoo.com>
2015-10-28 00:09:06 +0800

    Second

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-http.createServer(app).listen(8900);
+//http.createServer(app).listen(8900);
 
 app.get('/',function(req,res){
 	console.log('Listening to port 8900')
 	res.send('Hello From Express , Nepal');
 });
+
+module.exports = app;
