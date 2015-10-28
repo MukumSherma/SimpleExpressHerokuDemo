2613a1d1a174106dcdf7300afec31ccbb5d789cc
Mukum <laughingmukum@yahoo.com>
2015-10-28 10:33:42 +0800

    5th

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ var http = require('http');
 var app = express();
 
 //http.createServer(app).listen(8900);
+app.set('port', process.env.PORT || 5000);
+
+var server = app.listen(app.get('port'), function() {
+	console.log('Hello hello')
+  //debug('Express server listening on port ' + server.address().port);
+});
 
 app.get('/',function(req,res){
-	console.log('Listening to port 8900')
+	console.log('Listening to port 5000')
 	res.send('Hello From Express , Nepal');
 });
 
-module.exports = app;
+//module.exports = app;
